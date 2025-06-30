76efdce96b06943c7b83c85ab0158d0d285465e0
Collin Giess <giesscollin@gmail.com>
2025-06-29 23:54:47 -0400

    get rid of template garbage

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,7 +3,6 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 
-import * as middlewares from './middlewares';
 import api from './api';
 
 require('dotenv').config();
@@ -16,8 +15,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
 
 export default app;
